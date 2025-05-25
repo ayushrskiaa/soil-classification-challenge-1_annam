754c9a1ccdddf1711dc4555a9d8234807efa3226
Ayush Kumar <130389607+ayushrskiaa@users.noreply.github.com>
2025-05-25 21:01:26 +0530

    Add files via upload

--- challenge-1/src/postprocessing.py
+++ challenge-1/src/postprocessing.py
@@ -1,9 +1,8 @@
 """
 
 Author: Annam.ai IIT Ropar
-Team Name: SoilClassifiers
-Team Members: Member-1, Member-2, Member-3, Member-4, Member-5
-Leaderboard Rank: <Your Rank>
+Team Name: KrishiSetu
+Team Members: Dnyandeep Chute,Ayush Kumar, Suyash Mishra, Krish Kalgude, Yash Verma
 
 """
 
